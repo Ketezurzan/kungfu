1816dd2f62c4b33b257ca4d7f928652ed633af3c
Ketezurzan <ketezurzanlogin@gmail.com>
2025-02-05 13:23:07 -0500

    Create scripts.js
    number 3 mensajes

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por tu mensaje. Me pondré en contacto contigo pronto.');
+    // Aquí puedes agregar la lógica para enviar el formulario a tu correo electrónico o servidor
+});
